8fcbc20afb291fe4b496a3a7a5a11ad3f5e16da2
chaitu1 <chaitu143@gmail.com>
2024-02-29 17:22:23 +0530

    dummy coomit

--- printing-hello.sh
+++ printing-hello.sh
@@ -1,7 +1,17 @@
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+R="\e[31m"     # Red
+G="\e[32m"     # Green
+Y="\e[33m"     # Yellow
+B="\e[34m"     # Blue
+P="\e[35"      # Purple
+C="\e[36"      #
+Gr="\e[37"      # Gray
 
+N="\e[0m"      # End of colour modification
+
+# 1            # Bold
+# 4            # Underlined Characters
+# 5            # Blinking Characters
+ 
 echo "This is Batch-78"
 echo "This is Batch-78 DevOps Training"
 
@@ -9,4 +19,15 @@ echo "git How to do with colours"
 
 echo -e "$R This is Batch-78 $N"
 echo -e "$G This is Batch-78 DevOps Training $N"
+echo -e "$Y This is Batch-78 DevOps Training $N"
+echo -e "$B This is Batch-78 DevOps Training $N"
+echo -e "$P This is Batch-78 DevOps Training $N"
+echo -e "$C This is Batch-78 DevOps Training $N"
+echo -e "$Gr This is Batch-78 DevOps Training $N"
+
+
+
+
+
+
 
