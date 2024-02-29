40202828a1d49507ad016050b3cfbd0f47505787
chaitu1 <chaitu143@gmail.com>
2024-02-29 17:09:20 +0530

    First-file

---
+++ printing-hello.sh
@@ -0,0 +1,12 @@
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "This is Batch-78"
+echo "This is Batch-78 DevOps Training"
+
+echo "git How to do with colours"
+
+echo -e "$R This is Batch-78 $N"
+echo -e "$G This is Batch-78 DevOps Training $N"
+
